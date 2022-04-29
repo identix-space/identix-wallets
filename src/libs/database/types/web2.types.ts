3218764478aba46c1a6e65fb8ce4a715793dccd9
Dmitry Logunov <Dmitry.Logunov@nordigy.ru>
2022-04-29 19:53:45 +0200

    fix graphql enums

--- src/libs/database/types/web2.types.ts
+++ src/libs/database/types/web2.types.ts
@@ -1,3 +1,5 @@
+import { registerEnumType } from "@nestjs/graphql";
+
 export enum Web2AuthenticationMethods {
   clientId = 'clientId',
   magicLink = 'magicLink',
@@ -6,3 +8,7 @@ export enum Web2AuthenticationMethods {
   twitter = 'twitter',
   telegram = 'telegram'
 }
+
+registerEnumType(Web2AuthenticationMethods, {
+  name: 'Web2AuthenticationMethods',
+});
