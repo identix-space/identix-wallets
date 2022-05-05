189aac112164ddb6b880ec13890e48b5de2ebec6
Dmitry Logunov <Dmitry.Logunov@nordigy.ru>
2022-05-05 21:22:42 +0200

    fix dids

--- src/libs/common/helpers/strings.helpers.ts
+++ src/libs/common/helpers/strings.helpers.ts
@@ -6,3 +6,12 @@ export function underscoreToCamelCase(str: string): string {
   const parts = str.split(/[-_]/g);
   return parts[0].toLowerCase() + parts.slice(1).map(upFirst);
 }
+
+export const did = () => {
+  const alphaBettaNumber = ['a', 'b', 'c', 'e', 'f', 0, 1, 2, 3, 4, 5 ,6 , 7, 8, 9];
+  const address = [];
+  for(let i = 0; i < 64; i++) {
+    address.push(alphaBettaNumber[Math.floor( Math.random()*(alphaBettaNumber.length-1))])
+  }
+  return `did:ever:0:${address.join('')}`;
+}
